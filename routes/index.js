e93d63cd0d04d8363b18315205a51fba80ce631b
TOXICSAM\Samuel Santos <samuel.trabalho.96@gmail.com>
2019-01-24 17:59:44 +0000

    Sign Up with Passport:
    -Authentication Middleware:
    npm install --save passport
    
    -Hash Passwords:
    npm install --save bcrypt-nodejs
    
    -Session Storing Messages:
    npm install --save connect-flash
    
    -Strategy Authentication:
    npm install --save passport-local

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var Product = require('../models/product');
 var csrf = require('csurf');
+var passport = require('passport');
 
-//var csrfProtection = csrf({ cookie: true });
+var Product = require('../models/product');
+
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
@@ -22,11 +23,18 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/user/signup', (req, res, next) => {
-  res.render('user/signup', {csrfToken: req.csrfToken()});
+  var messages = req.flash('error');
+  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
-router.post('/user/signup', (req, res, next) => {
-  res.redirect('/');
+router.post('/user/signup', passport.authenticate('local.signup', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}));
+
+router.get('/user/profile', (req, res, next) => {
+  res.render('user/profile');
 });
 
 module.exports = router;
